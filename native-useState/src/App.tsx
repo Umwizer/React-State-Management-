7520d376b6e51d79046c4197a65e7fa7b20bd4b5
Umwizer <ruthumwizerwa@gmail.com>
2025-07-23 13:16:07 +0200

    add states

--- native-useState/src/App.tsx
+++ native-useState/src/App.tsx
@@ -1,9 +1,34 @@
 import { useState } from "react";
+function NameList() {
+  const [list, setList] = useState(["Ruth", "Jill", "John"]);
+  const [names, setName] = useState("");
+  const onAddName = () => {
+    if (names.trim() == "") return;
+    setList([...list, names]);
+    setName("");
+  };
+  return (
+    <div>
+      <ul>
+        {list.map((names) => (
+          <li key={names}>{names}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={names}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={onAddName}>AddName</button>
+    </div>
+  );
+}
 function App() {
   const [count, setCount] = useState(10);
   return (
     <div className="App">
       <button onClick={() => setCount(count + 1)}>Count = {count}</button>
+      <NameList />
     </div>
   );
 }
