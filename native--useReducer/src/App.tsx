1169df3668cdfee0584585ff122daabc8066fc9d
Umwizer <ruthumwizerwa@gmail.com>
2025-07-27 10:11:29 +0200

    native hooks

--- native--useReducer/src/App.tsx
+++ native--useReducer/src/App.tsx
@@ -1,35 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function UserForm() {
+  const [state, dispatch] = useReducer(
+    (state, action) => ({
+      ...state,
+      ...action,
+    }),
+    {
+      first: "",
+      last: "",
+    }
+  );
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <input
+        type="text"
+        value={state.first}
+        onChange={(e) => dispatch({ first: e.target.value })}
+      />
+      <input
+        type="text"
+        value={state.last}
+        onChange={(e) => dispatch({ last: e.target.value })}
+      />
+      <div>first:{state.first}</div>
+      <div>last:{state.last}</div>
+    </div>
+  );
 }
 
-export default App
+// Define state type
+type State = {
+  name: string;
+  names: string[];
+};
+
+// Define action types
+type Action = { type: "SET_NAME"; payload: string } | { type: "ADD_NAME" };
+
+// Reducer function with types
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "SET_NAME":
+      return { ...state, name: action.payload };
+    case "ADD_NAME":
+      return { ...state, names: [...state.names, state.name] };
+    default:
+      return state;
+  }
+};
+
+// Initial state
+const initialState: State = {
+  name: "",
+  names: [],
+};
+
+const NameList = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div className="App">
+      <div>
+        {state.names.map((name, index) => (
+          <div key={index}>{name}</div>
+        ))}
+      </div>
+
+      <input
+        type="text"
+        value={state.name}
+        onChange={(e) =>
+          dispatch({ type: "SET_NAME", payload: e.target.value })
+        }
+      />
+
+      <button onClick={() => dispatch({ type: "ADD_NAME" })}>Add Name</button>
+    </div>
+  );
+};
+function App() {
+  return (
+    <div>
+      <NameList />
+      <UserForm />
+    </div>
+  );
+}
+export default App;
