95f99a3dba5faaf9c61bd702706ff4047e328bab
Umwizer <ruthumwizerwa@gmail.com>
2025-07-31 19:14:01 +0200

    native

--- NATIVE-USEMEMOANDCALLBACK/src/App.tsx
+++ NATIVE-USEMEMOANDCALLBACK/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // This should be a string, not a string array
+  const [numbers] = useState("10,20,30");
+
+  // Memoized total as a number
+  const total = useMemo(() => {
+    const numberArray = numbers.split(",").map(Number);
+    return numberArray.reduce((acc, number) => acc + number, 0);
+  }, [numbers]);
+
+  const [names] = useState(["John", "Paul", "George", "Ringo"]);
+
+  // Memoized sortedNames as a string array
+  const sortedNames = useMemo(() => {
+    return [...names].sort();
+  }, [names]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Total: {total}</div>
+      <div>Names: {names.join(", ")}</div>
+      <div>SortedNames: {sortedNames.join(", ")}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
